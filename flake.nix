b3ca5d33c12991834c74c1c4a8127fb0a304af1c
t4ccer <t4ccer@gmail.com>
2023-02-07 17:48:56 -0700

    Use nix properly

--- flake.nix
+++ flake.nix
@@ -1,24 +1,54 @@
 {
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    pre-commit-hooks-nix = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs-stable.follows = "nixpkgs";
+    };
     crane.url = "github:ipetkov/crane";
-    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    crane,
-    utils,
-    ...
-  }: let
-    supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux"];
-  in
-    utils.lib.eachSystem supportedSystems
-    (
-      system: {
-        packages.scrolls = crane.lib.${system}.buildPackage {
-          src = self;
+
+  outputs =
+    inputs @ { self
+    , ...
+    }: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems =
+        if builtins.hasAttr "currentSystem" builtins
+        then [ builtins.currentSystem ]
+        else inputs.nixpkgs.lib.systems.flakeExposed;
+      flake.herculesCI.ciSystems = [ "x86_64-linux" ];
+      perSystem =
+        { config
+        , self'
+        , inputs'
+        , pkgs
+        , system
+        , ...
+        }: {
+          packages = {
+            scrolls = inputs.crane.lib.${system}.buildPackage {
+              src = self;
+            };
+            default = self'.packages.scrolls;
+          };
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = [
+              pkgs.cargo
+              pkgs.rustc
+              pkgs.cargo-outdated
+              pkgs.rustfmt
+              pkgs.rust-analyzer
+              pkgs.docker-compose
+              pkgs.redis
+            ];
+          };
+          formatter = pkgs.nixpkgs-fmt;
         };
-        packages.default = self.packages.${system}.scrolls;
-      }
-    );
+    };
 }
